f36ebc39bee847fa10a9ce1bd1db97b9044e6c0c
mctinez07 <64256629+mctinez07@users.noreply.github.com>
2020-04-29 13:21:49 +0200

    Add Inventory Form

---
+++ TrazinsAtenea/Forms/Inventory/InventoryForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace TrazinsAtenea.Forms.Inventory
+{
+    public partial class InventoryForm : DevExpress.XtraEditors.XtraForm
+    {
+        public InventoryForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
