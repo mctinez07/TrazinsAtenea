219ebc8932c629799ffb117739544aa841e46456
mctinez07 <64256629+mctinez07@users.noreply.github.com>
2020-04-29 12:24:31 +0200

    MainForm
    Create timer, main menu button and exit menu item.

--- TrazinsAtenea/MainForm.Designer.cs
+++ TrazinsAtenea/MainForm.Designer.cs
@@ -29,10 +29,201 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pnlMainHedaer = new DevExpress.XtraEditors.PanelControl();
+            this.pnlMainTimer = new DevExpress.XtraEditors.PanelControl();
+            this.lblMainTimer = new DevExpress.XtraEditors.LabelControl();
+            this.spcMainHeader = new DevExpress.XtraEditors.SeparatorControl();
+            this.btnMainMenu = new System.Windows.Forms.Button();
+            this.pcbLogo = new System.Windows.Forms.PictureBox();
+            this.dlmLoginInfo = new TrazinsAtenea.Controls.DoubleLabelMenu();
+            this.tmrMain = new System.Windows.Forms.Timer(this.components);
+            this.cmsExit = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.cmsMainMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsiInventory = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsiChangeUser = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsiExit = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlMainHedaer)).BeginInit();
+            this.pnlMainHedaer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlMainTimer)).BeginInit();
+            this.pnlMainTimer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.spcMainHeader)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLogo)).BeginInit();
+            this.cmsExit.SuspendLayout();
+            this.cmsMainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlMainHedaer
+            // 
+            this.pnlMainHedaer.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(227)))), ((int)(((byte)(243)))));
+            this.pnlMainHedaer.Appearance.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(227)))), ((int)(((byte)(243)))));
+            this.pnlMainHedaer.Appearance.BorderColor = System.Drawing.Color.SlateGray;
+            this.pnlMainHedaer.Appearance.Options.UseBackColor = true;
+            this.pnlMainHedaer.Appearance.Options.UseBorderColor = true;
+            this.pnlMainHedaer.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.Simple;
+            this.pnlMainHedaer.Controls.Add(this.pnlMainTimer);
+            this.pnlMainHedaer.Controls.Add(this.btnMainMenu);
+            this.pnlMainHedaer.Controls.Add(this.pcbLogo);
+            this.pnlMainHedaer.Controls.Add(this.dlmLoginInfo);
+            this.pnlMainHedaer.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlMainHedaer.Location = new System.Drawing.Point(0, 0);
+            this.pnlMainHedaer.LookAndFeel.Style = DevExpress.LookAndFeel.LookAndFeelStyle.Flat;
+            this.pnlMainHedaer.LookAndFeel.UseDefaultLookAndFeel = false;
+            this.pnlMainHedaer.Name = "pnlMainHedaer";
+            this.pnlMainHedaer.Size = new System.Drawing.Size(1384, 54);
+            this.pnlMainHedaer.TabIndex = 1;
+            // 
+            // pnlMainTimer
+            // 
+            this.pnlMainTimer.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.pnlMainTimer.Controls.Add(this.lblMainTimer);
+            this.pnlMainTimer.Controls.Add(this.spcMainHeader);
+            this.pnlMainTimer.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlMainTimer.Location = new System.Drawing.Point(982, 2);
+            this.pnlMainTimer.Name = "pnlMainTimer";
+            this.pnlMainTimer.Size = new System.Drawing.Size(245, 50);
+            this.pnlMainTimer.TabIndex = 5;
+            // 
+            // lblMainTimer
+            // 
+            this.lblMainTimer.Appearance.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMainTimer.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(76)))), ((int)(((byte)(90)))));
+            this.lblMainTimer.Appearance.Options.UseFont = true;
+            this.lblMainTimer.Appearance.Options.UseForeColor = true;
+            this.lblMainTimer.Location = new System.Drawing.Point(30, 10);
+            this.lblMainTimer.Name = "lblMainTimer";
+            this.lblMainTimer.Size = new System.Drawing.Size(126, 27);
+            this.lblMainTimer.TabIndex = 3;
+            this.lblMainTimer.Text = "labelControl1";
+            // 
+            // spcMainHeader
+            // 
+            this.spcMainHeader.Dock = System.Windows.Forms.DockStyle.Right;
+            this.spcMainHeader.LineAlignment = DevExpress.XtraEditors.Alignment.Far;
+            this.spcMainHeader.LineColor = System.Drawing.Color.SlateGray;
+            this.spcMainHeader.LineOrientation = System.Windows.Forms.Orientation.Vertical;
+            this.spcMainHeader.Location = new System.Drawing.Point(221, 0);
+            this.spcMainHeader.Name = "spcMainHeader";
+            this.spcMainHeader.Padding = new System.Windows.Forms.Padding(0, 0, 9, 0);
+            this.spcMainHeader.Size = new System.Drawing.Size(24, 50);
+            this.spcMainHeader.TabIndex = 6;
+            // 
+            // btnMainMenu
+            // 
+            this.btnMainMenu.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnMainMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMainMenu.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMainMenu.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(76)))), ((int)(((byte)(90)))));
+            this.btnMainMenu.Image = global::TrazinsAtenea.Properties.Resources.ic_arrow_drop_down;
+            this.btnMainMenu.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnMainMenu.Location = new System.Drawing.Point(65, 1);
+            this.btnMainMenu.Name = "btnMainMenu";
+            this.btnMainMenu.Size = new System.Drawing.Size(379, 52);
+            this.btnMainMenu.TabIndex = 1;
+            this.btnMainMenu.Text = "Trazins Atenea";
+            this.btnMainMenu.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMainMenu.UseVisualStyleBackColor = true;
+            // 
+            // pcbLogo
+            // 
+            this.pcbLogo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pcbLogo.Location = new System.Drawing.Point(2, 2);
+            this.pcbLogo.Name = "pcbLogo";
+            this.pcbLogo.Size = new System.Drawing.Size(62, 50);
+            this.pcbLogo.TabIndex = 0;
+            this.pcbLogo.TabStop = false;
+            // 
+            // dlmLoginInfo
+            // 
+            this.dlmLoginInfo.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dlmLoginInfo.Location = new System.Drawing.Point(1227, 2);
+            this.dlmLoginInfo.Name = "dlmLoginInfo";
+            this.dlmLoginInfo.Size = new System.Drawing.Size(155, 50);
+            this.dlmLoginInfo.TabIndex = 6;
+            // 
+            // tmrMain
+            // 
+            this.tmrMain.Enabled = true;
+            this.tmrMain.Interval = 1000;
+            this.tmrMain.Tick += new System.EventHandler(this.tmrMain_Tick);
+            // 
+            // cmsExit
+            // 
+            this.cmsExit.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmsExit.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsiChangeUser,
+            this.tsiExit});
+            this.cmsExit.Name = "cmsExit";
+            this.cmsExit.Size = new System.Drawing.Size(249, 90);
+            // 
+            // cmsMainMenu
+            // 
+            this.cmsMainMenu.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmsMainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsiInventory});
+            this.cmsMainMenu.Name = "cmsMainMenu";
+            this.cmsMainMenu.Size = new System.Drawing.Size(249, 48);
+            // 
+            // tsiInventory
+            // 
+            this.tsiInventory.Name = "tsiInventory";
+            this.tsiInventory.Size = new System.Drawing.Size(248, 44);
+            this.tsiInventory.Text = "Inventario";
+            // 
+            // tsiChangeUser
+            // 
+            this.tsiChangeUser.Name = "tsiChangeUser";
+            this.tsiChangeUser.Size = new System.Drawing.Size(167, 22);
+            this.tsiChangeUser.Text = "Cambiar de usuar";
+            // 
+            // tsiExit
+            // 
+            this.tsiExit.Name = "tsiExit";
+            this.tsiExit.Size = new System.Drawing.Size(167, 22);
+            this.tsiExit.Text = "tsiExit";
+            // 
+            // MainForm
+            // 
+            this.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(227)))), ((int)(((byte)(243)))));
+            this.Appearance.Options.UseBackColor = true;
+            this.Appearance.Options.UseFont = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1384, 756);
+            this.Controls.Add(this.pnlMainHedaer);
+            this.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MainForm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pnlMainHedaer)).EndInit();
+            this.pnlMainHedaer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pnlMainTimer)).EndInit();
+            this.pnlMainTimer.ResumeLayout(false);
+            this.pnlMainTimer.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.spcMainHeader)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLogo)).EndInit();
+            this.cmsExit.ResumeLayout(false);
+            this.cmsMainMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private DevExpress.XtraEditors.PanelControl pnlMainHedaer;
+        private DevExpress.XtraEditors.PanelControl pnlMainTimer;
+        private DevExpress.XtraEditors.LabelControl lblMainTimer;
+        private DevExpress.XtraEditors.SeparatorControl spcMainHeader;
+        private System.Windows.Forms.Button btnMainMenu;
+        private System.Windows.Forms.PictureBox pcbLogo;
+        private Controls.DoubleLabelMenu dlmLoginInfo;
+        private System.Windows.Forms.Timer tmrMain;
+        private System.Windows.Forms.ContextMenuStrip cmsExit;
+        private System.Windows.Forms.ContextMenuStrip cmsMainMenu;
+        private System.Windows.Forms.ToolStripMenuItem tsiInventory;
+        private System.Windows.Forms.ToolStripMenuItem tsiChangeUser;
+        private System.Windows.Forms.ToolStripMenuItem tsiExit;
     }
 }
--- TrazinsAtenea/MainForm.cs
+++ TrazinsAtenea/MainForm.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
+using TrazinsAtenea.Controls;
 
 namespace TrazinsAtenea
 {
@@ -15,7 +16,146 @@ namespace TrazinsAtenea
     {
         public MainForm()
         {
-            InitializeComponent();
+            InitializeComponent();            
+
+            dlmLoginInfo.SetValues("María García", "TAP");
+
+            lblMainTimer.Text = DateTime.Now.ToString();
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            LoadHeaderMenu(btnMainMenu, cmsMainMenu);
+            LoadHeaderMenu(dlmLoginInfo, cmsExit);
+        }
+
+        #region Gestión Botones menú principal 
+
+        private void LoadHeaderMenu(Control ctrl, ContextMenuStrip cms)
+        {
+            //Asignar la posición del menú.
+            if (ctrl is Button)
+            {
+                ctrl.Click -= (s, e) => SetMenuPosition(ctrl);
+                ctrl.Click += (s, e) => SetMenuPosition(ctrl);
+            }
+
+            if (ctrl is DoubleLabelMenu)
+            {
+                var control = (DoubleLabelMenu)ctrl;
+                control.Click -= (s, e) => SetMenuPosition(ctrl);
+                control.Click += (s, e) => SetMenuPosition(ctrl);
+                control.OnLabelClick -= (s, e) => SetMenuPosition(ctrl);
+                control.OnLabelClick += (s, e) => SetMenuPosition(ctrl);
+            }
+
+            cms.AutoSize = true;
+            cms.ShowImageMargin = false;
+
+            ctrl.ContextMenuStrip = cms;
+
+            foreach (ToolStripMenuItem item in cms.Items)
+            {
+                item.DisplayStyle = ToolStripItemDisplayStyle.Text;
+
+                //Cambiar color de los items.
+                item.ForeColor = Color.FromArgb(63, 76, 90);
+
+                item.Padding = new Padding(5);
+
+                //Obtener la traducción del elemento                
+                item.Text = GetResource(item.Name);
+
+                item.Click += new EventHandler(ContextMenuItem_Click);
+                item.TextAlign = ContentAlignment.BottomLeft;
+
+                if (cms.Items.Count > 0 && ctrl is Button)
+                {
+                    item.AutoSize = false;
+                    item.Height = btnMainMenu.Height;
+                    item.Width = btnMainMenu.Width;
+                    cms.AutoSize = false;
+                    cms.Height = (cms.Items[0].Height * (cms.Items.Count) + 4);
+                    cms.Width = btnMainMenu.Width;
+                }
+                else if (ctrl is DoubleLabelMenu)
+                {
+                    item.Height = ctrl.Height;
+                    item.Width = ctrl.Width;
+                    cms.AutoSize = true;
+                }
+                else
+                {
+                    cms.Size = Size.Empty;
+                }
+
+            }
+
+        }
+
+        private void SetMenuPosition(Control crtl)
+        {
+            if (crtl.ContextMenuStrip != null)
+            {
+
+                Point Posicion = new Point();
+
+                Posicion.X = crtl.Parent.Left + crtl.Left;
+                Posicion.Y = crtl.Parent.Top + crtl.Height;
+
+                crtl.ContextMenuStrip.Show(this, Posicion);
+            };
+        }
+
+        //Obtiene el recurso para el multilenguaje.
+        //Poner en bases. Modificarlo para cualquier control??
+        //El valor del tag lo puedo usar para saber que recurso coger.
+        private string GetResource(string value)
+        {
+            switch (value)
+            {
+                case "tsiChangeUser":
+                    return GlobalResources.TrazinsAtenea.ChangeUser;
+                case "tsiExit":
+                    return GlobalResources.TrazinsAtenea.Exit;
+                case "tsiInventory":
+                    return GlobalResources.TrazinsAtenea.Inventory;
+                default:
+                    return string.Empty;
+            }
+        }
+
+        private void ContextMenuItem_Click(object sender, EventArgs e)
+        {
+            SubMenuItemClick((ToolStripMenuItem)sender);
+        }
+
+        private void SubMenuItemClick(ToolStripMenuItem element)
+        {
+            switch (element.Name)
+            {
+                case "tliInventory":
+                    //Cambiar por la traducción del recurso                    
+                    btnMainMenu.Text = GlobalResources.TrazinsAtenea.Inventory;
+                    //OpenForm(new frmInventory());
+                    break;
+                case "tliExit":
+                    MessageBox.Show("Exit");
+                    //OpenForm(new frmWashes());
+                    break;
+                case "tliChange":
+                    MessageBox.Show("ChangeUser");
+                    break;
+                default:
+                    break;
+            };
+        }
+
+        #endregion
+
+        private void tmrMain_Tick(object sender, EventArgs e)
+        {
+            lblMainTimer.Text = DateTime.Now.ToString();
         }
     }
 }
